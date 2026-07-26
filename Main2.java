0faee5793389472b20318fe9f11bee8e011ec543
hemalogesh41-cloud <hemalogesh41@gmail.com>
2026-07-26 10:40:30 -0400

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+for(int i = 0; i<n; i++);
+    a[n] = sc.nextInt();
+for(int i=0; i<n; i++);
+    b[n] = sc.nextInt();
+    boolean same = true;
+for(int i=0; i<n; i++){
+    if(a[i] == b[i]){
+           same = false;
+           break;
+    }       
+  }
+if(same)
+System.out.println("Array are same");
+else  
+System.out.println("Array are not same");  
+}
+}
